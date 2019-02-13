1b7672fc7fac03bdaf9567a116786368ee6e0b95
Rizky Dwi Hidayat <rizky@alphatech.id>
2019-02-13 19:53:16 +0700

    edited

--- ATA_Blog/models.py
+++ ATA_Blog/models.py
@@ -7,7 +7,7 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     gambar = models.ImageField()
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.CharField(max_length=255)
     judul = models.CharField(max_length=255)
     isi = models.TextField()
 
